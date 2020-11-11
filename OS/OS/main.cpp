fa7bcba0d6c8df0a23dfeb5c4783e65d73ada958
chandra.mouli <chandra.mouli@servicenow.com>
2020-11-11 09:46:01 +0530

    cpu spin loop

---
+++ OS/OS/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OS
+//
+//  Created by Chandra on 11/11/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
